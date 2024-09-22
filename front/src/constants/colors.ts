0b26a7bf74b605dc1450fc7880f0d3efe4e5cda4
eprnf <eprnf21@gmail.com>
2024-09-23 06:50:41 +0900

    add: 컬러 상수 추가

---
+++ front/src/constants/colors.ts
@@ -0,0 +1,16 @@
+const colors = {
+  BLUE: '#0A84FF', //
+  PINK_500: '#BF5C79',
+  PINK_700: '#C63B64',
+  RED_300: '#FFB4B4',
+  RED_500: '#FF5F5F',
+  WHITE: '#FFF',
+  GRAY_200: '#E7E7E7',
+  GRAY_500: '#8E8E8E',
+  GRAY_600: '#757577', //
+  GRAY_700: '#575757',
+  GRAY_900: '#1C1C1E', //
+  BLACK: '#000',
+};
+
+export {colors};
