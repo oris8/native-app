7b0ac8962846ade6baac1fa3f31f14410efa6f24
eprnf <eprnf21@gmail.com>
2024-09-23 07:07:52 +0900

    fix typo

--- front/src/constants/index.ts
+++ front/src/constants/index.ts
@@ -1,7 +1,2 @@
-const authNavigation = {
-  AUTH_HOME: 'AuthHome',
-  LOGIN: 'Login',
-  SIGNUP: 'Signup',
-} as const;
-
-export {authNavigation};
+export * from './colors';
+export * from './navigation';
